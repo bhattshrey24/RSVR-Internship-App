dc688d55cd6c8e95333ab8352e38574754578df5
bhattshrey24 <bhatt.shrey1224@gmail.com>
2022-07-31 17:45:30 +0530

    Checking project restructure

--- app/src/main/java/com/example/rsvpinternshipapp/MainActivity.kt
+++ app/src/main/java/com/example/rsvpinternshipapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater, null, false)
     }
-
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
